1b62199fbe895a5fe64c6de4e2f44940c2854417
Neel Wankhade <neelwankhade007@gmail.com>
2026-03-15 15:52:08 +0530

    Update MongoDB connection string to use environment variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoDB");
+mongoose.connect(process.env.MONGO_URI);
 
 const Task = require('./models/Task');
 
